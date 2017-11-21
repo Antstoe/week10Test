2ee8451dac4c4a5ce53a62af2cfa33eb042d1cac
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-11-20 21:19:49 -0700

    finished activities 25-27

--- 26-Inquirer_Users/Unsolved/userprompt.js
+++ 26-Inquirer_Users/Unsolved/userprompt.js
@@ -1,3 +1,5 @@
+
+
 // Create a basic command line Node application using the inquirer package.
 // Your application should ask the user any five questions of your choosing.
 // The question set should include at least one:
@@ -14,3 +16,41 @@
 // Remember to be creative!
 
 // ========================================================================
+var inquirer = require('inquirer');
+inquirer.prompt([
+    {
+    type: "input",
+    name: "favPoke",
+    message: "What pokemon is your favorite?"
+    },
+    {
+        type: "password",
+        name: "pw",
+        message: "Give your pokemon a pw"
+    },
+    {
+        type: "list",
+        message: "Choose your second pokemon",
+        choices: ["pikachu", "charzard", "abra", "porygon"],
+        name: "chosenOne"
+    },
+    {
+     type: "checkbox",
+     name: "type",
+     message: "If you were the avatar what would your starting bender power be?",
+     choices: ["earth", "fire", "water", "Wind"]
+    },
+    {
+        type: "confirm",
+        name: "final",
+        message: "Are you sure about your choices?"
+
+    }
+
+])
+.then(function(data){
+    console.log(data);
+    if (data.pw === "wows") {
+        console.log("wow your favorite pokemon is " + data.favPoke + ". Its awesome that you really like " + data.chosenOne + ". being a " + data.type + " bender would rule!!!" );
+    }
+})
--- 27-Inquirer_Geocode/Unsolved/inquirerGeocode.js
+++ 27-Inquirer_Geocode/Unsolved/inquirerGeocode.js
@@ -9,28 +9,31 @@
 
 // Include the geocoder NPM package (Remember to run "npm install geocoder"!)
 var geocoder = require("geocoder");
+var inquirer = require('inquirer');
+inquire.prompt([
+  {
+  type: "input",
+  message: "Where do you want to search (place of interest or City, State)",
+  name: "place",
 
-// Take in the command line arguments
-var nodeArgs = process.argv;
+}])
+.then(function (data) {
+  
+  // Take in the command line arguments
+   data.place;
+  
+  // Create an empty string for holding the address
+  var address = "";
+  
+  // Capture all the words in the address (again ignoring the first two Node arguments)
+ 
+  // Then use the Google Geocoder to geocode the address
+  geocoder.geocode(address, function(err, data) {
+  
+    // Then console log the result and stringify it.
+    // Note the argument of "2" being included in the JSON stringify. This makes the JSON output pretty.
+    // See link here: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+    console.log(JSON.stringify(data, null, 2));
+  });
 
-// Create an empty string for holding the address
-var address = "";
-
-// Capture all the words in the address (again ignoring the first two Node arguments)
-for (var i = 2; i < nodeArgs.length; i++) {
-
-  // Build a string with the address.
-  address = address + "" + nodeArgs[i];
-  console.log(address);
-
-}
-
-// Then use the Google Geocoder to geocode the address
-geocoder.geocode(address, function(err, data) {
-
-  // Then console log the result and stringify it.
-  // Note the argument of "2" being included in the JSON stringify. This makes the JSON output pretty.
-  // See link here: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
-  console.log(JSON.stringify(data, null, 2));
-});
-
+})  
